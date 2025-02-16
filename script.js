011d5f293bebb134145b104d2922a374029bec9a
bernardkrehula <krehula.bernard456@gmail.com>
2025-02-16 18:16:29 +0100

    Added css

--- script.js
+++ script.js
@@ -1,67 +1,71 @@
-let ul = document.querySelector('.to-do-list');
-let input = document.querySelector('input');
-let btn = document.querySelector('.btn');
-let main = document.querySelector('.main');
-let list = document.querySelector('.main li');
+let ul = document.querySelector(".to-do-list");
+let input = document.querySelector("input");
+let btn = document.querySelector(".btn");
+let main = document.querySelector(".main");
+let list = document.querySelector(".main li");
 let toDoInput;
 let id;
 let activeId;
 let activeIndex;
-let newToDoValue;
 
 function toDoManager() {
     let toDoArray = [];
     const getIndex = (currentManagerId) => {
         currentManagerId = toDoArray.find((element) => {
-            return element.id == activeId;
-        })
+        return element.id == activeId;
+        });
         activeIndex = toDoArray.indexOf(currentManagerId);
-    }
+    };
     const addToDo = (toDo) => {
         toDoArray.push(toDo);
-    }
+    };
     const getToDos = () => {
         return toDoArray;
-    }
+    };
     const deleteText = () => {
-        toDoArray.splice(activeIndex, 1)
-    }
-    return {addToDo, getToDos , deleteText, getIndex};
+        toDoArray.splice(activeIndex, 1);
+    };
+    return { addToDo, getToDos, deleteText, getIndex };
 }
 const manager = toDoManager();
-input.addEventListener('input', (e) => {
+
+input.addEventListener("input", (e) => {
     toDoInput = e.target.value;
-})
-btn.addEventListener('click', () => {
-    const newToDo = {id: crypto.randomUUID(), value: toDoInput};
+});
+
+btn.addEventListener("click", () => {
+    const newToDo = { id: crypto.randomUUID(), value: toDoInput };
     manager.addToDo(newToDo);
     id = newToDo.id;
     newToDoValue = newToDo.value;
     makeNewBtn(id, newToDoValue);
-})
-
+    removeInputValue()
+});
 function makeNewBtn(id, newToDoValue) {
-    const html = `<button class="dugme" id="${id}">${value}</button>`;
-    list = document.querySelector('.main li');
-    list.insertAdjacentHTML('beforeend', html)
+    const html = `<button class="dugme" id="${id}">${newToDoValue}</button>`;
+    list = document.querySelector(".main li");
+    list.insertAdjacentHTML("beforeend", html);
 }
 
-main.addEventListener('click', (event) => {
-    if(event.target.className == 'dugme') {
-        removeBtn(event)
+main.addEventListener("click", (event) => {
+    if (event.target.className == "dugme") {
+        removeBtn(event);
         activeId = event.target.id;
         manager.getIndex();
         manager.deleteText();
+        console.log(manager.getToDos())
     }
-})
+});
 
 function removeBtn(event) {
-    list = document.querySelector('.main li');
+    list = document.querySelector(".main li");
     let currentBtn = event.target;
     list.removeChild(currentBtn);
 }
 
-
+function removeInputValue() {
+    input.value = '';
+}
 
 /* function counterCreator() {
     let count = 0;
